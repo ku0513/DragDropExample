de887f7bfb8b7d57d9f2b61a2cdf2d3b1c47772d
ku0513 <kenichi.umeda@gmail.com>
2021-12-29 01:08:17 +0900

    レクチャー18 (ドロップ後の並び替えを実装しよう)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './App.css';
 
 function App() {
+  const [items, setItem] = useState(["item0", "item1", "item2"])
+
+  const onDragEnd = (result) => {
+    const remove = items.splice(result.source.index, 1);
+    console.log(remove);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="doppable">
           {(provided) =>
             <div
@@ -16,9 +24,28 @@ function App() {
                 ref={provided.innerRef}
                 {...provided.draggableProps}
                 {...provided.dragHandleProps}>
-                  item0
+                  {items[0]}
                 </div>}
               </Draggable>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) =>
+                <div className="item"
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}>
+                  {items[1]}
+                </div>}
+              </Draggable>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) =>
+                <div className="item"
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}>
+                  {items[2]}
+                </div>}
+              </Draggable>
+              {provided.placeholder}
             </div>
           }
         </Droppable>
